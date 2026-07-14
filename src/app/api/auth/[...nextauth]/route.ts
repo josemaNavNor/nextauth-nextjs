1eee13c2127d58edbaa5389ccfe6d7862ec2b553
josemaNavNor <navvajosen@gmail.com>
2026-07-14 07:21:17 -0700

    add Microdoft Azure Authentication

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,17 @@
 import NextAuth from "next-auth";   
 import GoogleProvider from "next-auth/providers/google";
+import AzureADProvider from "next-auth/providers/azure-ad";
 
 const authOptions = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+        }),
+        AzureADProvider({
+            clientId: process.env.AZURE_AD_CLIENT_ID!,
+            clientSecret: process.env.AZURE_AD_CLIENT_SECRET!,
+            tenantId: process.env.AZURE_AD_TENANT_ID!
         })
     ]
 });
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
 function DashboardPage() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className="text-4xl font-bold">Welcome to NextAuth.js!</h1>
+            <h1 className="text-4xl font-bold">DASHBOARRRDDDD</h1>
             <p className="mt-4 text-lg text-gray-600">
-                This is the home page of your Next.js application with NextAuth.js authentication.
+                BIENVENIDOS AL DASHBOARD DE NEXTAUTH.JS, AHORA PUEDES INICIAR SESION CON GOOGLE O MICROSOFT AZURE AD.
             </p>
         </main>
     );
